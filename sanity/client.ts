b5c9498bfec803f75ff51ee0124fe0f713c4300f
brkpod-oss <brkpod-oss@users.noreply.github.com>
2026-08-13 19:56:57 +0530

    feat: add Sanity client, env and image url helpers

---
+++ sanity/client.ts
@@ -0,0 +1,17 @@
+import { createClient } from "next-sanity";
+import { projectId, dataset, apiVersion } from "./env";
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+});
+
+export const writeClient = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false,
+  token: process.env.SANITY_API_TOKEN,
+});
